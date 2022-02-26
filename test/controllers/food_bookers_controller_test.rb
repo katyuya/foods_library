dba88c0751b9ff64eb7cd794f30949cedd7515c3
katyuya <aka850259@gmail.com>
2022-02-26 13:31:13 +0000

    [fin]foods_library test　完了

---
+++ test/controllers/food_bookers_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FoodBookersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/food_booker_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FoodBookerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
