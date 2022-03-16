77f4806daa3561e8d4df4792ea48e369c3f0a5e7
katyuya <aka850259@gmail.com>
2022-03-16 16:16:00 +0000

    [add]ルートの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :food_bookers, :controllers => {
     :sessions => 'food_bookers/sessions',
     :registrations => 'food_bookers/registrations',
   }
 
   root to: 'homes#top'
-  resources :food_bookers, only:[:show, :edit, :update]
+  resources :food_bookers, only: [:show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
     resources :book_comments, only: [:create, :destroy]
   end
